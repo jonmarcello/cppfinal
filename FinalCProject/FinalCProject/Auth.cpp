b05424197af1605814cb15a85035338d7c902651
Administrator <admin@example.com>
2018-08-02 23:55:34 -0400

    Initial commit

---
+++ FinalCProject/FinalCProject/Auth.cpp
@@ -0,0 +1,85 @@
+#pragma once
+#include "User.cpp"
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Auth {
+public:
+	Auth() {}
+
+	User loginUser() {
+
+	}
+
+	User getUserById(int id) {
+		int number_of_lines = 0;
+		string strId = to_string(id);
+		string line;
+		User u;
+		vector<int> vect;
+		ifstream authFile("users.csv");
+		vector<string> explodedLine;
+
+		while (getline(authFile, line)) {
+			explodedLine = explode(line, ',');
+
+			if (explodedLine[0] == strId) {
+				u.setUsername(explodedLine[1]);
+				u.setEncryptedPassword(explodedLine[2]);
+				break;
+			}
+		}
+
+		return u;
+	}
+
+	void createUser(User newUser) {
+		ofstream authFile;
+		authFile.open("users.csv", ios_base::app);
+		authFile << to_string(numberOfUsers()) + "," + newUser.get_username() + "," + newUser.get_password() + "\n";
+		authFile.close();
+	}
+
+private:
+	vector<string> explode(const string& str, const char& ch) {
+		string next;
+		vector<string> result;
+
+		// For each character in the string
+		for (string::const_iterator it = str.begin(); it != str.end(); it++) {
+			// If we've hit the terminal character
+			if (*it == ch) {
+				// If we have some characters accumulated
+				if (!next.empty()) {
+					// Add them to the result vector
+					result.push_back(next);
+					next.clear();
+				}
+			}
+			else {
+				// Accumulate the next character into the sequence
+				next += *it;
+			}
+		}
+		if (!next.empty())
+			result.push_back(next);
+		return result;
+	}
+
+	int numberOfUsers() {
+		int number_of_lines = 0;
+		string line;
+		ifstream authFile("users.csv");
+
+		while (getline(authFile, line))
+			++number_of_lines;
+
+		return number_of_lines;
+	}
+
+	vector<string> splitAtComma() {
+
+	}
+};
---
+++ FinalCProject/FinalCProject/User.cpp
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class User
+{
+
+public:
+	User() {}
+	
+	// constructor method
+	User(const string &user, const string &password) {
+		setUsername(user);
+		setPassword(password);
+	}
+
+	void setUsername(string name) {
+		username = name;
+	}
+
+	void setPassword(string pass) {
+		passwordhash = encrypt(pass);
+	}
+
+	void setEncryptedPassword(string pass) {
+		passwordhash = pass;
+	}
+
+	// accessors
+	const string get_username()
+	{
+		return username;
+	}
+
+	const string get_password()
+	{
+		return passwordhash;
+	}
+
+private:
+	string username;
+	string passwordhash;
+
+	string encrypt(string toEncrypt) {
+		char key[3] = { 'K', 'C', 'Q' }; //Any chars will work, in an array of any size
+		string output = toEncrypt;
+
+		for (int i = 0; i < toEncrypt.size(); i++)
+			output[i] = toEncrypt[i] ^ key[i % (sizeof(key) / sizeof(char))];
+
+		return output;
+	}
+
+
+	// Rewritten to use const& on both parameters
+	string decrypt(string msg)
+	{
+		return encrypt(msg);
+	}
+
+
+};
